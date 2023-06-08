6c329c900e818ec3d46bc236c4ecdf568b3ff1e7
sahil <sahil@DESKTOP-92BED6E.domain.name>
2023-06-08 11:02:43 +0530

    project done

--- src/components/Login.js
+++ src/components/Login.js
@@ -32,15 +32,16 @@ function Login(props) {
     <div>
       <form action="" onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email address</label>
-          <input type="email" className="form-control" value={credentials.email} name='email' id="email" onChange={onChange} placeholder="name@example.com" />
+          <h2>Login To Continue</h2>
+          <label htmlFor="email" className="form-label my-2">Email address</label>
+          <input type="email" className="form-control my-2" value={credentials.email} name='email' id="email" onChange={onChange} placeholder="name@example.com" />
         </div>
-        <label htmlFor="password" className="form-label">Password</label>
-        <input type="password" name="password" id="password" value={credentials.password} className="form-control" onChange={onChange} aria-labelledby="passwordHelpBlock" />
-        <div id="labelpassword" className="form-text">
+        <label htmlFor="password" className="form-label my-2">Password</label>
+        <input type="password" name="password" id="password" value={credentials.password} className="form-control my-2" onChange={onChange} aria-labelledby="passwordHelpBlock" />
+        <div id="labelpassword" className=" my-1form-text">
           Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
         </div>
-        <button className="btn btn-primary">Login</button>
+        <button className="btn btn-primary my-2">Login</button>
       </form>
     </div>
   )
